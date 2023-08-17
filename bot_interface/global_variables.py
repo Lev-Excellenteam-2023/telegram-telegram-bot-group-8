c3eede49278df535db4cca0444208d4ed1cd7821
hodaya12 <hodayaroz1@gmail.com>
2023-08-17 11:18:55 +0300

    add global variables

---
+++ bot_interface/global_variables.py
@@ -0,0 +1,8 @@
+chat_id_finish=[]
+name_of_doctor={}
+user_answers = {}  # Store answers for each user
+questions = ['How would you describe the dentist\'s demeanor and attitude towards you during the visit?',
+'how likely are you to recommend this dentist to a friend or family member?',
+'How well did the dentist manage your pain and discomfort during the treatment?',
+'How would you rate the dentist\'s knowledge and expertise in the field of dentistry?',
+'Did the dentist follow proper hygiene and safety protocols during your visit?']

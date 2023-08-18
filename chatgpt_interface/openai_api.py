31d75157be7a378877db17bd6099b1706289d585
rivkaadler1 <adlerrivka66@gmail.com>
2023-08-18 16:49:33 +0300

    changes in classOpenAIChatAPI:

--- chatgpt_interface/openai_api.py
+++ chatgpt_interface/openai_api.py
@@ -1,5 +1,5 @@
-import os
 import openai
+import os
 
 
 class OpenAIChatAPI:
@@ -8,12 +8,13 @@ class OpenAIChatAPI:
         openai.api_key = os.getenv("OPENAI_API_KEY")
 
     async def generate_response(self, prompt: str):
-        completion = openai.Completion.create(
+        completion = await openai.ChatCompletion.acreate(
             model="gpt-3.5-turbo",
-            prompt=prompt,
-            max_tokens=1
+            messages=prompt,
+            max_tokens=50
         )
-        chat_response = completion.choices[0].text.strip()
+
+        chat_response = completion.choices[0].message.content
         return chat_response
 
     def set_system_role(self, role: str):
